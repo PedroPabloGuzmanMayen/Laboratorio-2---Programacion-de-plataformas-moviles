21b64710906f864a47c7b8ca3ab0b6518d968df2
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-07-27 22:21:54 -0600

    Final

--- Lab2/src/main/kotlin/Map function.kt
+++ Lab2/src/main/kotlin/Map function.kt
@@ -1,5 +1,7 @@
 fun mapping(list: List<String>, extra: String):List<String> = list.map{"$extra, $it!"}
 fun main(){
     val arr = listOf("Andre", "Juan","Carlos")
-    println(mapping(arr, "!Hola"))
+    println("Ingresa el mensaje que quieres combinar con la lista: ")
+    val word: String? = readLine()
+    println(mapping(arr, word.toString()))
 }
--- Lab2/src/main/kotlin/prueba.kt
+++
@@ -1,6 +0,0 @@
-import kotlin.random.Random
-
-fun main(){
-    val random = Random(System.currentTimeMillis())
-    println(random.nextInt(100_000, 1_000_000))
-}
